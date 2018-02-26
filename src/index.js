f45e821c2e259c0abcad081fa2c1d7154145d485
geek10010 <36638017+geek10010@users.noreply.github.com>
2018-02-26 18:39:55 +0300

    src/index.js brackets function 1 version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0) return false;
+  let arr = [];
+  for (let i=0; i<str.length; i++) {
+    if (str[i] === bracketsConfig[0][0]) {
+      arr.push(str[i]);
+    }
+    if (str[i] === bracketsConfig[0][1]) {
+      arr.pop(str[i]);
+    }
+  }
+  
+  if (arr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
