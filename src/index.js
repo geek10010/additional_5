5406c8a530e79314e579960a509f07de2adfda48
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-09-26 21:13:13 +0300

    refactor check function

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  
-  let strLength = str.length;
-  let checkStack = [];
-  let bracketsPairs = {};
-  let currentBracket;
-  let previousBracket;
+  const strLength = str.length;
+  const checkStack = [];
+  const bracketsPairs = {};
 
   if (strLength % 2 > 0) {
     return false;
   }
-  
-  for (let i = 0; i < bracketsConfig.length; i++) {
-    bracketsPairs[bracketsConfig[i][0]] = bracketsConfig[i][1];
-  }
-  
-  for (let i = 0; i < strLength; i++) {
-    currentBracket = str[i];
-    previousBracket = checkStack[checkStack.length - 1];
-    
+
+  bracketsConfig.forEach((element) => {
+    const [openBracket, closeBracket] = element;
+    bracketsPairs[openBracket] = closeBracket;
+  });
+
+  for (let i = 0; i < strLength; i += 1) {
+    const currentBracket = str[i];
+    const previousBracket = checkStack[checkStack.length - 1];
+
     if (currentBracket === bracketsPairs[previousBracket]) {
       checkStack.pop();
     } else {
       checkStack.push(currentBracket);
     }
   }
-  
+
   return checkStack.length === 0;
 };
